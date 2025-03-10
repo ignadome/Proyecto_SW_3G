4033030992084dd72df7e30ba5b75c2c3225e39f
cesarxtoo <145981291+cesarxtoo@users.noreply.github.com>
2025-03-10 15:37:40 +0100

    Nuevas llamadas a la bbdd e intento de desarrollar la parte de registro (hay un error no se donde)

--- src/users/User.js
+++ src/users/User.js
@@ -6,23 +6,23 @@ export const RolesEnum = Object.freeze({
 });
 
 export class User {
+
     static #getByUsernameStmt = null;
     static #insertStmt = null;
     static #updateStmt = null;
-
+    static #getLastIDStmt = null;
+    static #getByIdStmt = null;
     static initStatements(db) {
         if (this.#getByUsernameStmt !== null) return;
 
         this.#getByUsernameStmt = db.prepare('SELECT * FROM user WHERE username = @username');
-        this.#insertStmt = db.prepare('INSERT INTO user(username, bio, password,  profile_picture, user_type) VALUES (@username, @bio, @password, @profile_picture, @user_type)');
+        this.#getByIdStmt('SELECT * FROM user WHERE id = @id');
+        this.#insertStmt = db.prepare('INSERT INTO user(username, bio, password,  profile_picture, user_type,id) VALUES (@username, @bio, @password, @profile_picture, @user_type,@id)');
         this.#updateStmt = db.prepare('UPDATE user SET username = @username, bio=@bio, password = @password,  profile_picture=@profile_picture, user_type=@user_type WHERE id = @id');
+        this.#getLastIDStmt = db.prepare('SELECT MAX(id) FROM user');
     }
-    static postNewUser(username) {
-        const user = this.#getByUsernameStmt.get({ username });
-        if (user === undefined) throw new userNotFound(username);
-        const { bio, password,  profile_picture, user_type } = user;
-
-        return new User(username, bio, password,  profile_picture, user_type);
+    static getNextId(){
+        return this.#getLastIDStmt.run() + 1; //La idea es que tome el mayor valor de id en la bbdd y le sume 1
     }
     static getUserByUsername(username) {
        
@@ -32,20 +32,21 @@ export class User {
 
         return new User(username, bio, password,  profile_picture, user_type);
     }
-
+     
     static #insert(user) {
         let result = null;
         try {
             const username = user.#username;
-            const bio= user.bio;
             const password = user.#password;
-            const  profile_picture=user.profile_picture;
-            const user_type = user.#user_type;
-            const datos = {username, bio, password, profile_picture, user_type};
+            const bio = user.#bio;
+            const role = RolesEnum.USER; //Por defecto voy a poner que meta usuario con rol normal y supondre que son los admins los que lo cambian
+            const profile_picture = user.#profile_picture;
+            const id = this.getNextId();
+            const datos = {username,bio, password,profile_picture, role,id};
 
             result = this.#insertStmt.run(datos);
 
-            user.#id = result.lastInsertRowid;
+            //user.#id = result.lastInsertRowid; No estoy seguro de que hacer con esto
         } catch(e) { // SqliteError: https://github.com/WiseLibs/better-sqlite3/blob/master/docs/api.md#class-sqliteerror
             if (e.code === 'SQLITE_CONSTRAINT') {
                 throw new userAlreadyExists(user.#username);
@@ -72,11 +73,12 @@ export class User {
     static register(username, password) {
         let user = null;
             user = this.getUserByUsername(username);
-            if(user !== null) throw new userAlreadyExists(username); //Si el usuario ya existe, entonces no puedes registrarlo
+            if(user !== undefined) throw new userAlreadyExists(username); //Si el usuario ya existe, entonces no puedes registrarlo
             else{
-                user = new User(username,null,password,null,USER,null); //Queda hacer un sistema de asignacion de ids
-                this.#insert(user);
+                user = new User(username,null,password,null,USER,0);
+                user = this.#insert(user);
             }
+        if(this.#getByIdStmt.run(user.#id)) throw new userNotRegistered(); //Compruebo si el usuario ha podido ser metido en la tabla
         return user;
     }
 
@@ -96,18 +98,17 @@ export class User {
 
     #id;
     #username;
-    bio;
+    #bio;
     #password;
-     profile_picture;
+    #profile_picture;
     #user_type;
     
-
-    constructor(username, bio, password,  profile_picture, user_type = RolesEnum.user, id = null) {
+    constructor(username,bio,password,profile_picture,user_type,id){
         this.#username = username;
-        this.bio=bio;
+        this.#bio = bio;
         this.#password = password;
-        this.profile_picture= profile_picture;
-        this.#user_type=user_type;
+        this.#profile_picture = profile_picture;
+        this.#user_type = user_type;
         this.#id = id;
     }
 
@@ -175,5 +176,17 @@ export class userAlreadyExists extends Error {
     constructor(username, options) {
         super(`user ya existe: ${username}`, options);
         this.name = 'userAlreadyExists';
+    }
+}
+
+export class userNotRegistered extends Error {
+    /**
+     * 
+     * @param {string} username 
+     * @param {ErrorOptions} [options]
+     */
+    constructor(username, options) {
+        super(`Something happened, try again later`, options);
+        this.name = 'userNotRegistered';
     }
 }
--- src/users/router.js
+++ src/users/router.js
@@ -1,12 +1,11 @@
 import express from 'express';
-import { viewLogin, doLogin, doLogout,viewLogin,doSignUp } from './controller.js';
+import { viewLogin, doLogin, doLogout, doRegister,viewRegister } from './controller.js';
 
 const usersRouter = express.Router();
 
 usersRouter.get('/login', viewLogin);
 usersRouter.post('/login', doLogin);
 usersRouter.get('/logout', doLogout);
-usersRouter.get('/signUp',viewSignUp);
-usersRouter.post('/signUp',doSignUp);
-
+usersRouter.post('/login', doRegister);
+usersRouter.get('/login',viewRegister)
 export default usersRouter;
