4193de93238f2ee12c739db8e9245947163bf1eb
maelhg01 <maelhg01@ucm.es>
2025-03-14 13:12:18 +0100

    Añadido filtrados de busqueda lista juegos y modificar juegos por admins

--- src/games/controller.js
+++ src/games/controller.js
@@ -28,8 +28,37 @@ export function showGameListSearched(req, res){
     let contenido = 'pages/listajuegos';
 
     const title = req.body.game_title.trim();
+    const order_option = req.body.order_option;
+    let order;
+    switch(order_option){
+        case "optionAlfabetico":
+            order = "title";
+        break;
+        case "optionNota":
+            order = "rating";
+        break;
+        case "optionNumFavoritos":
+            order = "favNumber";
+        break;
+        default:
+            order = "id";
+        break;
+    }
 
-    const gameList = Game.getSearchedGameList(title, 50, 0);
+    const asc_desc_option = req.body.asc_desc_option;
+    let order_dir;
+    switch(asc_desc_option){
+        case "optionDesc":
+            order_dir = 'DESC';
+        break;
+        case "optionAsc":
+            order_dir = 'ASC';
+        break;
+        default:
+            order_dir = 'ASC';
+    }
+
+    const gameList = Game.getSearchedGameList(title, order, order_dir, 50, 0);
 
     console.log("GAME LIST SEARCHED");
     console.log(gameList);
@@ -98,6 +127,61 @@ export function doAddGameBD(req, res){
         res.render('page', {
             contenido: 'pages/addGamePage',
             error: 'ERROR al insertar juego en la Base de Datos'
+        })
+    }
+}
+
+export function viewModifyGameBD(req, res){
+
+    //let id = req.params.id;
+    //const game = Game.getGameById(id);
+    //const game = req.body.game;
+    const gameId = req.params.id;
+    
+    const game = Game.getGameById(gameId);
+
+    console.log("Juego a modificar", game);
+
+    let contenido = 'pages/modifyGamePage';
+
+    res.render('page', {
+        contenido,
+        session: req.session,
+        game: game
+    });
+}
+
+
+export function doModifyGameBD(req, res){
+    const title = req.body.title.trim();
+    const gameId = req.params.id;
+    const description = req.body.description.trim();
+    const rating = Number(req.body.rating.trim());
+    const favNumber = parseInt(req.body.favNumber.trim());
+    const company_id = parseInt(req.body.company_id.trim());
+
+    try {
+        const new_info_game = new Game(title, description,rating, favNumber, "elden_ring.png" ,company_id, null);
+
+        console.log("Nueva info de juego");
+        console.log(new_info_game);
+
+        const game2 =  Game.update(gameId, new_info_game);
+        console.log("Juego modificado bien", game2);
+
+        return res.render('page', {
+            contenido: 'pages/modifyGamePage',
+            session: req.session,
+            game: game2,
+            exito: 'Juego modificado con exito en la Base de Datos'
+        });
+
+    } catch (e) {
+        res.render('page', {
+            contenido: 'pages/modifyGamePage',
+            session: req.session,
+            game: Game.getGameById(gameId),
+            error: 'ERROR al modificar juego en la Base de Datos'
         })
     }
 }
